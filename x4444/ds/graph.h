e1e77268feffd5da6a106e57987ab02fdf699a85
Alexander Pivovarov <apivovarov@gmail.com>
2024-11-30 19:21:05 -0800

    Add DFS pre/postorder to Graph

--- x4444/ds/graph.h
+++ x4444/ds/graph.h
@@ -46,6 +46,38 @@ class Graph {
       }
     }
   }
+
+  template <typename Callable>
+  void __dfs_preorder(int n, std::vector<bool>& visited, Callable func) {
+    if (visited[n]) return;
+    visited[n] = true;
+    func(n);
+    for (auto ch : adjList[n]) {
+      __dfs_preorder(ch, visited, func);
+    }
+  }
+
+  template <typename Callable>
+  void dfs_preorder(int src_node, Callable func) {
+    std::vector<bool> visited(adjList.size());
+    __dfs_preorder(src_node, visited, func);
+  }
+
+  template <typename Callable>
+  void __dfs_postorder(int n, std::vector<bool>& visited, Callable func) {
+    if (visited[n]) return;
+    visited[n] = true;
+    for (auto ch : adjList[n]) {
+      __dfs_postorder(ch, visited, func);
+    }
+    func(n);
+  }
+
+  template <typename Callable>
+  void dfs_postorder(int src_node, Callable func) {
+    std::vector<bool> visited(adjList.size());
+    __dfs_postorder(src_node, visited, func);
+  }
 };
 
 }  // namespace ds
--- x4444/ds/graph_main.cc
+++ x4444/ds/graph_main.cc
@@ -8,20 +8,34 @@ struct Printer {
 
 int main() {
   x4444::ds::Graph g;
-  g.push_back({1, 2});
-  g.push_back({0, 2, 3});
-  g.push_back({0, 1, 4});
-  g.push_back({1, 4});
-  g.push_back({2, 3});
+  g.push_back({1, 2});           // 0
+  g.push_back({0, 2, 3, 7});     // 1
+  g.push_back({0, 1, 4, 5, 7});  // 2
+  g.push_back({1, 4, 6, 7});     // 3
+  g.push_back({2, 3, 7});        // 4
+  g.push_back({2});              // 5
+  g.push_back({3, 8});           // 6
+  g.push_back({1, 2, 3, 4});     // 7
+  g.push_back({6});              // 8
 
+  std::cerr << "BFS(3):" << std::endl;
   g.bfs(3, Printer<int>());
   std::cerr << std::endl;
+  std::cerr << "BFS2(3):" << std::endl;
   g.bfs2(3, Printer<int>());
   std::cerr << std::endl;
 
   const char * s = "Node";
+  std::cerr << "BFS(3):" << std::endl;
   g.bfs(3, [s](int v) { std::cerr << s << ":" << v << ","; });
   std::cerr << std::endl;
+  std::cerr << "BFS2(3):" << std::endl;
   g.bfs2(3, [s](int v) { std::cerr << s << ":" << v << ","; });
+  std::cerr << std::endl;
+  std::cerr << "DFS Preorder:" << std::endl;
+  g.dfs_preorder(0, [](int v) { std::cerr << v << ","; });
+  std::cerr << std::endl;
+  std::cerr << "DFS Postorder:" << std::endl;
+  g.dfs_postorder(0, [](int v) { std::cerr << v << ","; });
   std::cerr << std::endl;
 }
