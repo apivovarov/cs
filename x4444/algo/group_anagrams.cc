b4897366012f46d68ff224edc1b9947cd51f4d07
Alexander Pivovarov <apivovarov@gmail.com>
2024-12-07 13:58:22 -0800

    Handle edge cases in group anagrams

--- x4444/algo/group_anagrams.cc
+++ x4444/algo/group_anagrams.cc
@@ -10,16 +10,18 @@ std::string getKey(std::string s) {
   return s;
 }
 
-std::vector<std::vector<std::string>> process(std::vector<std::string>& arr) {
+std::vector<std::vector<std::string>> group_anagrams(const std::vector<std::string>& arr) {
+  if (arr.empty()) return {};
+  if (arr.size() == 1) return {{arr[0]}};
   // insert to hashmap is O(1)
   std::unordered_map<std::string, std::vector<std::string>> map(arr.size());
-  for (auto& s : arr) {
+  for (const auto& s : arr) {
     map[getKey(s)].push_back(s);
   }
 
   std::vector<std::vector<std::string>> res;
   // scan hashmap and move underlying vector to res container - O(n)
-  for (auto& key_vec : map) {
+  for (const auto& key_vec : map) {
     if (!key_vec.second.empty()) {
       res.push_back(std::move(key_vec.second));
     }
@@ -27,18 +29,23 @@ std::vector<std::vector<std::string>> process(std::vector<std::string>& arr) {
   return res;
 }
 
-int main() {
-  std::vector<std::string> arr = {"foo", "bar",   "ofo", "bra",
-                                  "ab",  "hello", "ba"};
-  std::vector<std::vector<std::string>> res = process(arr);
+void dump(const std::vector<std::vector<std::string>> & res) {
   std::cout << "[";
   for (const auto& vec : res) {
     std::cout << "[";
     for (const auto& s : vec) {
       std::cout << s << ",";
     }
-    std::cout << "]";
+    std::cout << "],";
   }
   std::cout << "]";
   std::cout << std::endl;
+}
+
+int main() {
+  std::vector<std::string> arr = {"foo", "bar",   "ofo", "bra",
+                                  "ab",  "hello", "ba"};
+  std::vector<std::vector<std::string>> res = group_anagrams(arr);
+  dump(res);
+  dump(group_anagrams(std::vector<std::string>{"foo"}));
 }
