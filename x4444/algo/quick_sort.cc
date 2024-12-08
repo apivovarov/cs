103d25535b3aec8d26aa3028c87ed27fe311fc63
Alexander Pivovarov <apivovarov@gmail.com>
2024-12-07 17:19:37 -0800

    Add Quick Sort with random pivot point

--- x4444/algo/quick_sort.cc
+++ x4444/algo/quick_sort.cc
@@ -1,24 +1,74 @@
-#include <vector>
+#include <chrono>
+#include <cstdlib>
 #include <iostream>
+#include <vector>
 
-int partition(std::vector<int> & arr, int low, int high) {
+int partition(std::vector<int>& arr, int low, int high) {
+  int piv_id = low + std::rand() % (high - low);
+  std::swap(arr[piv_id], arr[high - 1]);
   int piv = arr[high - 1];
   int i = low - 1;
-  for (int j = low; j < high; ++j) {
+  for (int j = low; j < high - 1; ++j) {
     if (arr[j] < piv) {
-        std::swap(arr[++i], arr[j]);
+      std::swap(arr[++i], arr[j]);
     }
   }
-  std::swap(arr[++i], arr[high-1]);
+  std::swap(arr[++i], arr[high - 1]);
   return i;
 }
 
-int main() {
-    std::vector<int> a = {1,5,4,2,3};
-    int piv_id = partition(a, 0, a.size());
+void _qsort(std::vector<int>& arr, int low, int high) {
+  int sz = high - low;
+  if (sz < 2) return;
+  int piv = partition(arr, low, high);
+  _qsort(arr, low, piv);
+  _qsort(arr, piv + 1, high);
+}
 
-    std::cout<< piv_id << std::endl;
-    for (const auto & v : a)
-      std::cout<< v << ",";
-    std::cout<<std::endl;
+void qsort(std::vector<int>& arr) {
+  int low = 0;
+  int high = arr.size();
+  _qsort(arr, low, high);
+}
+
+int main2() {
+  std::vector<int> a = {1, 5, 9,  4,  2,  10, 3, 1, 5,  9, 2, 9,
+                        6, 2, 12, 11, 5,  2,  2, 9, 9,  4, 2, 1,
+                        6, 7, 11, 10, 12, 8,  1, 0, -4, 1, 9};
+  qsort(a);
+  for (const auto& v : a) std::cout << v << ",";
+  std::cout << std::endl;
+  return 0;
+}
+
+int main() {
+  std::vector<int> a;
+  a.reserve(1024);
+  int num;
+  while (std::cin >> num) {
+    a.push_back(num);
+  }
+  // std::vector<int> b = a;
+
+  std::cerr << "size: " << a.size() << std::endl;
+
+  auto start = std::chrono::high_resolution_clock::now();
+  qsort(a);
+  // std::sort(a.begin(), a.end());
+  auto end = std::chrono::high_resolution_clock::now();
+  auto duration =
+      std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+
+  std::cerr << "Execution time: " << duration.count() << " micro sec"
+            << std::endl;
+
+  // std::sort(b.begin(), b.end());
+  // std::cerr << std::boolalpha << (a == b);
+
+  std::cout << "min/max: " << a.front() << ".." << a.back() << std::endl;
+
+  // for (const auto & v : a)
+  //   std::cout<< v << ",";
+  // std::cout<<std::endl;
+  return 0;
 }
