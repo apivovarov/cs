672043af469c333f8d22633c0b780cd5124f0c56
Alexander Pivovarov <apivovarov@gmail.com>
2024-12-19 20:29:18 -0800

    Optimize Product Except Self code

--- x4444/prob/prod_except_self.cc
+++ x4444/prob/prod_except_self.cc
@@ -8,40 +8,13 @@ class Solution {
     if (nums.size() == 1) return nums;
     if (nums.size() == 2) return {nums[1], nums[0]};
 
-    int prod = 1;
-    int zero_id = -1;
-    int zero_cnt = 0;
     std::vector<int> res(nums.size());
     res[0] = 1;
-    for (int i = 0; i < nums.size(); ++i) {
-      if (nums[i] == 0) {
-        ++zero_cnt;
-        zero_id = i;
-      } else {
-        prod *= nums[i];
-      }
-      if (i == 0) {
-        continue;
-      }
-      if (nums[i - 1] == 0) {
-        res[i] = res[i - 1];
-      } else {
-        res[i] = res[i - 1] * nums[i - 1];
-      }
+    for (int i = 1; i < nums.size(); ++i) {
+      res[i] = res[i - 1] * nums[i - 1];
     }
 
-    if (zero_cnt > 1) {
-      std::vector<int> res(nums.size());
-      return res;
-    }
-    if (zero_cnt == 1) {
-      std::vector<int> res(nums.size());
-      res[zero_id] = prod;
-      return res;
-    }
-
-    prod = 1;
-
+    int prod = 1;
     for (int i = nums.size() - 2; i >= 0; --i) {
       prod *= nums[i + 1];
       res[i] *= prod;
