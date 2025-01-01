93a94bc1018ee9a1e04f165e0fb0dbaaf7b13b08
Alexander Pivovarov <apivovarov@gmail.com>
2024-12-31 23:45:14 -0800

    A bit Optimize summary_ranges.cc

--- x4444/prob/summary_ranges.cc
+++ x4444/prob/summary_ranges.cc
@@ -12,7 +12,7 @@ class Solution {
     int b = nums[0];
     int e = nums[0];
     for (int i = 1; i < nums.size(); ++i) {
-      if (nums[i] == nums[i - 1] + 1) {
+      if (nums[i] == e + 1) {
         e = nums[i];
         continue;
       }
