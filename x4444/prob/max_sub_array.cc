f347bdf06f7a1ff6f4053f0b9b43663bdc44d1ec
Alexander Pivovarov <apivovarov@gmail.com>
2025-01-13 15:34:27 -0800

    Optimize Maximum Subarray problem

--- x4444/prob/max_sub_array.cc
+++ x4444/prob/max_sub_array.cc
@@ -1,24 +1,19 @@
 #include <iostream>
 #include <vector>
-#include <numeric>
 
+// 53. Maximum Subarray
 class Solution {
  public:
   int maxSubArray(const std::vector<int>& arr) {
     if (arr.empty()) return 0;
     if (arr.size() == 1) return arr[0];
     int sum = 0;
-    int maxSum = 0;
-    int maxEl = arr[0];
+    int maxSum = -1e6;
     for (int i = 0; i < arr.size(); ++i) {
       sum += arr[i];
-      if (sum < 0)
-        sum = 0;
-      else if (sum > maxSum)
-        maxSum = sum;
-      if (arr[i] > maxEl) maxEl = arr[i];
+      if (sum > maxSum) maxSum = sum;
+      if (sum < 0) sum = 0;
     }
-    if (maxSum == 0) return maxEl;
     return maxSum;
   }
 };
